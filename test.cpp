ec463321555845b8b0b1afb444efebf7348f0738
Matheus Fabio Raimundo Da Silva <fabiomatheus488@gmail.com>
2022-08-23 20:29:06 -0300

    chore: Some changes

--- test.cpp
+++ test.cpp
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <locale.h>
 
-int main()
-{
+//Inicio do algoritmo
+int main(){
+	
+	setlocale (LC_ALL, "Portuguese");
+	
+//  Declaraçao de variaveis	
     float x1,x2,y1,y2,d;
     
-    printf("Digite o valor  de x1 e x2: \n");
+    printf("Digite o valor de x1 e x2: \n");
 	scanf("%f %f", &x1,&x2);
 	
-	printf("Digite o valor  de y1 e y2: \n");
+	printf("Digite o valor de y1 e y2: \n");
 	scanf("%f %f", &y1,&y2);
     
+//  Processamento de dados    
     d=sqrt (pow(x2-x1, 2) + pow (y2-y1, 2));
     
-    printf("O valor da distacia = %.2f  \n", d);
+//  Saida de dados    
+    printf("O valor da distacia é = %.0f  \n", d);
 	
     system("pause");
 	return 0;
